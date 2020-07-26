4dab1e9aa833f53beff7a7ce3a45e9fc19245ba3
vishchi <vishalsc1999@gmail.com>
2020-07-26 14:28:41 +0530

    Final Commit

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+﻿
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "₹0 to ₹1000",
+        "array": [0, 1000]
+    },
+    {
+        "_id": 2,
+        "name": "₹1001 to ₹5000",
+        "array": [1001, 5000]
+    },
+    {
+        "_id": 3,
+        "name": "₹5001 to ₹10000",
+        "array": [5001, 10000]
+    },
+    {
+        "_id": 4,
+        "name": "₹10001 to ₹15000",
+        "array": [10001, 150000]
+    },
+    {
+        "_id": 5,
+        "name": "More than ₹15000",
+        "array": [15000, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Mobile"
+    },
+    {
+        "_id": 2,
+        "name": "Laptop"
+    },
+    {
+        "_id": 3,
+        "name": "Computer Peripherals"
+    },
+    {
+        "_id": 4,
+        "name": "Tablet"
+    },
+    {
+        "_id": 5,
+        "name": "Mobile Accessories"
+    },
+    {
+        "_id": 6,
+        "name": "Camera"
+    },
+    {
+        "_id": 7,
+        "name": "Speakers"
+    }
+]
+
+
+export {
+    price,
+    continents
+}
---
+++ server/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://vishal:vishboi@cluster0.skpel.mongodb.net/test?retryWrites=true&w=majority'
+}
